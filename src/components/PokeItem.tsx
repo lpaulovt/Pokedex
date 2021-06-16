e9d69949540891224b18095b392abc24ea3b04ac
caioRafael <caiorafaelrg@gmail.com>
2021-06-16 00:53:28 -0300

    montagem pokeItem

--- src/components/PokeItem.tsx
+++ src/components/PokeItem.tsx
@@ -1,7 +1,19 @@
+import { PokeType } from './PokeType';
+
 import styles from '../styles/components/PokeItem.module.css';
 
 export function PokeItem(){
   return(
-    <div className={styles.pokeContainer}></div>
+    <div className={styles.pokeContainer}>
+      <div>
+      <h1>#001</h1>
+      <h1>Bulbasaur</h1>
+      <span>
+        <PokeType/>
+        <b style={{background:'#a240a0', padding: 3, borderRadius: 5, color:'#fff'}}>Poison</b>
+      </span>
+      </div>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon" />
+    </div>
   );
 }
---
+++ src/components/PokeType.tsx
@@ -0,0 +1,8 @@
+export function PokeType(){
+  return(
+    <>
+      <b style={{background:'#2cc02a', padding: 3, borderRadius: 5, color:'#fff'}}>Grass</b>
+      {' '}
+    </>
+  )
+}
