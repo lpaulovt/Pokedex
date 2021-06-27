a9819927d40b9195cd218b4095a9346aee38b580
caioRafael <caiorafaelrg@gmail.com>
2021-06-26 23:06:02 -0300

    bug requisicao

--- src/components/PokeList.tsx
+++ src/components/PokeList.tsx
@@ -1,8 +1,33 @@
 import { PokeItem } from './PokeItem';
 
 import styles from '../styles/components/PokeList.module.css'
+import { pokeAPI } from '../services/pokeAPI';
+import { GetStaticProps } from 'next';
+import { useEffect } from 'react';
 
-export function PokeList(){
+type Pokemon = {
+  entry_number: number,
+  pokemon_species: object,
+}
+
+type Props = {
+  pokelist: Pokemon[];
+}
+
+export function PokeList({props}:any){
+  console.log(props)
+
+  // async function handlePokeList(){
+  //   const response = await pokeAPI.get('pokedex/2');
+
+  //   const pokelist = response.data.pokemon_entries;
+  
+  //   console.log(pokelist);
+  // }
+  // useEffect(()=>{
+  //   handlePokeList();
+  // },[])
+
   return(
     <div className={styles.contentList}>
       <div className={styles.screen}>
@@ -18,4 +43,20 @@ export function PokeList(){
       </div>
     </div>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await pokeAPI.get('pokedex/2/');
+
+  const {pokemon_entries} = response.data;
+  const pokelist = pokemon_entries;
+
+  console.log(response.data)
+
+  return {
+    revalidate: 5,
+    props: {
+      pokelist: response.data
+    }
+  }
 }
